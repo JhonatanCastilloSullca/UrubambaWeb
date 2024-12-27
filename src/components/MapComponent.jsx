969a0062d1c7524da66592fc5f17a334744b3ae3
Jhonatan Castillo Sullca <jcastillosullca@gmail.com>
2024-12-27 11:15:10 -0500

    1712115

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -7,20 +7,20 @@ import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import TileWMS from 'ol/source/TileWMS';
 
-const direcionServicioWMS= 'http://192.168.3.10:81/servicio/wms',
-direcionServicioMapCache= 'http://192.168.3.10:81/cgi-bin/mapcache/?',formatoPNG= 'image/png',
-formatoJPEG= 'image/jpeg',
-formatoJson= 'application/json',
-formatoText= 'text/html',
-formatoGeoJson= 'geojson',
-versionWMS= '1.3.0';
-const sectores = new TileLayer({source: new TileWMS({url: direcionServicioWMS, params: {'LAYERS': 'sectores', 'TILED': true, 'FORMAT': formatoPNG, VERSION: versionWMS}, serverType: 'mapserver', transition: 0}), title: 'Sectores', visible: true});
-const lotes = new TileLayer({source: new TileWMS({url: direcionServicioWMS, params: {'LAYERS': 'lotes', 'TILED': true, 'FORMAT': formatoPNG, VERSION: versionWMS}, serverType: 'mapserver', transition: 0}), title: 'Lotes', visible: false});
+const direcionServicioWMS = 'http://192.168.3.10:81/servicio/wms',
+    direcionServicioMapCache = 'http://192.168.3.10:81/cgi-bin/mapcache/?', formatoPNG = 'image/png',
+    formatoJPEG = 'image/jpeg',
+    formatoJson = 'application/json',
+    formatoText = 'text/html',
+    formatoGeoJson = 'geojson',
+    versionWMS = '1.3.0';
+const sectores = new TileLayer({ source: new TileWMS({ url: direcionServicioWMS, params: { 'LAYERS': 'sectores', 'TILED': true, 'FORMAT': formatoPNG, VERSION: versionWMS }, serverType: 'mapserver', transition: 0 }), title: 'Sectores', visible: true });
+const lotes = new TileLayer({ source: new TileWMS({ url: direcionServicioWMS, params: { 'LAYERS': 'lotes', 'TILED': true, 'FORMAT': formatoPNG, VERSION: versionWMS }, serverType: 'mapserver', transition: 0 }), title: 'Lotes', visible: false });
 
 const MapComponent = () => {
     useEffect(() => {
         const map = new Map({
-            target: "map", // Asegúrate de que este id exista en el HTML
+            target: "map",
             layers: [
                 new TileLayer({
                     source: new OSM(),
@@ -35,7 +35,7 @@ const MapComponent = () => {
         });
 
         return () => {
-            // Limpia el mapa al desmontar
+
             map.setTarget(null);
         };
     }, []);
